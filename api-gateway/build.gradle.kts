c007e6a339c247f87b34c5e43fc1dc61640960ae
a.mironnikov <amironnikov@telematika.com>
2025-09-04 08:56:49 +0300

    Add Api-Gateway to docker-compose.yml

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     id("java")
+    id ("org.springframework.boot") version "3.5.4"
+    id ("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "ru.amironnikov"
-version = "1.0.o"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -13,6 +15,7 @@ var cloudVersion = "4.3.0"
 
 dependencies {
 
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.cloud:spring-cloud-starter-gateway:$cloudVersion")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:$cloudVersion")
 }
